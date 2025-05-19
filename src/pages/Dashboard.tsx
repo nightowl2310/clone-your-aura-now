a38747f1499bc79f9e26b6cc5d9c846e2f1f1801
kavyansh <kavyanshsaxena23@gmail.com>
2025-05-19 19:09:45 +0530

    try fix

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -69,6 +69,9 @@ const Dashboard = () => {
               <SentimentChart />
             </div>
             <div>
+              <ReplyTimeChart />
+            </div>
+            <div>
               <AccuracyChart />
             </div>
             <div className="lg:col-span-2">
